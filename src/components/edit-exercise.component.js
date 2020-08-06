128f211c9d4a0aa6748ef1f6537fe8640c0df7ed
Luong Cong Dan <lcd11001@gmail.com>
2020-08-06 15:53:35 +0700

    + simple form add new Exercise + simple form add new User

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class EditExercise extends React.Component {
+    render() {
+        return (
+            <div>
+                EditExercise
+            </div>
+        )
+    }
+}
---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ExerciseList extends React.Component {
+    render() {
+        return (
+            <div>
+                ExerciseList
+            </div>
+        )
+    }
+}
